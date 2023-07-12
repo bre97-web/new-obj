49f7898d9b05f8470e5af1d9bed5ef5b600542e6
goodgostudyddup <97076932+goodgostudyddup@users.noreply.github.com>
2023-07-12 09:48:27 +0800

    1
    1

--- src/main/java/com/controller/Controller.java
+++ src/main/java/com/controller/Controller.java
@@ -3,12 +3,14 @@ package com.controller;
 import com.pojo.User;
 import com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 @CrossOrigin
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -16,9 +18,14 @@ public class Controller {
     UserService userService;
 
     /**
-     * 返回数据库user表中全部数据
-     * @return
+     * 接口名:selectAll
+     * 请求方式 :post
+     * 需要参数:无
+     * 返回类型 List<Map<String, Object>>
+     * 用途:返回数据库user表中全部数据
+     *示例:http://localhost:8080/selectAll
      */
+
     @RequestMapping(value = "selectAll", method = RequestMethod.POST)
     @ResponseBody
     public List<Map<String, Object>> test() {
@@ -27,8 +34,12 @@ public class Controller {
     }
 
     /**
-     * 根据前端传入u_id 删除对应数据
-     * @param u_id
+     * 接口名 :deleteUser
+     * 请求方式:get
+     * 需要参数:u_id
+     * 用途:根据ID删除用户
+     * 返回类型:boolean
+     * 示例:http://localhost:8080/deleteUser?u_id=???
      */
     @RequestMapping(value = "deleteUser")
     @ResponseBody
@@ -38,10 +49,12 @@ public class Controller {
     }
 
     /**
-     * 增添数据
-     *
-     * @param user
-     * @return boole
+     *接口名:addUser
+     * 请求方式:get
+     * 返回类型:Boolean
+     * 参数:  pojo.User
+     * 用途:添加一条记录
+     * 示例:http://localhost:8080/addUser?u_id=?&u_name=?&u_pwd=?
      */
     @RequestMapping(value = "addUser")
     @ResponseBody
@@ -51,10 +64,14 @@ public class Controller {
     }
 
     /**
+     * 接口名:alterUser
+     * 请求方式 get
+     * 返回类型 boolean
+     * 参数:pojo.User
+     * 用途:修改记录数据
      * @param user u_id 需要修改的数据的id
      *             u_name 修改后的name
      *             u_pwd 修改后的pwd
-     * @return boolean
      */
     @RequestMapping(value = "alterUser")
     @ResponseBody
