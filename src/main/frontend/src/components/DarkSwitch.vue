ad9c1d47ab83825f765c634013e8cd57634ab3fe
bre97-web <by.sgatn3@gmail.com>
2023-07-12 14:21:03 +0800

    DarkSwitch component

---
+++ src/main/frontend/src/components/DarkSwitch.vue
@@ -0,0 +1,27 @@
+<template>
+    <md-standard-icon-button @click="toggleDark">
+        <md-icon>light_mode</md-icon>
+        <!-- <md-icon>dark_mode</md-icon> -->
+    </md-standard-icon-button>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+
+const isDark = ref(false)
+const setDark = (e: boolean) => {
+    isDark.value = e
+}
+const toggleDark = () => {
+    setDark(!isDark.value)
+}
+
+watch(isDark, () => {
+    document.documentElement.classList[isDark.value ? 'remove': 'add']('dark')
+})
+</script>
+
+<style scoped>
+
+</style>
