efa0357168b7cd2bcf566ba1301c133b78f663ae
bre97-web <by.sgatn3@gmail.com>
2023-07-13 20:06:07 +0800

    Update TopNavigation.vue

--- src/main/frontend/src/components/TopNavigation.vue
+++ src/main/frontend/src/components/TopNavigation.vue
@@ -1,8 +1,10 @@
 <template>
     <nav class="flex flex-row items-center justify-between px-4 py-2 bg-[var(--md-sys-color-background)]">
-        <OpenNavigationRailButton></OpenNavigationRailButton>
-
-        <Title></Title>
+        <div class="flex items-center gap-4">
+            <OpenNavigationRailButton></OpenNavigationRailButton>
+            
+            <Title></Title>
+        </div>
         
         <nav class="flex items-center justify-end">
             <Menu v-slot="{ open }">
@@ -26,7 +28,8 @@ import OpenNavigationRailButton from './OpenNavigationRailButton.vue';
 
 const Title = () => (
     <div>
-        <h1>记账小工具</h1>
+        <h1 class="font-semibold">记账小工具</h1>
+        <p class="text-xs">减少你的心智负担</p>
     </div>
 )
 </script>
